36f280b4c6c627ca9f6aaf37efcd69453c6b61ef
Moises <moisesprotazio@gmail.com>
2025-01-01 17:53:47 -0300

    Create the project

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+
+fun main() = runBlocking { //it's possible to create own scope with coroutineScope
+    println("Welcome to CookFast  your app to manage orders")
+
+    launch {
+
+    }
+
+
+}
